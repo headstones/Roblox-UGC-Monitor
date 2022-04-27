6d5e74e3b0b5a0744827be17d4c3d43e9a013b72
chloe <75399033+headstones@users.noreply.github.com>
2022-04-26 23:01:28 -0500

    Update notifer.py

--- notifer.py
+++ notifer.py
@@ -3,16 +3,16 @@ import json
 import time
 import discord
 from discord import Webhook, RequestsWebhookAdapter
-# the holy grail of  variables
-discordWebhook = "https://discord.com/api/webhooks... webhook stuff" # ONLY ONE U NEED TO CHANGE 
-####### dont touch anything below this unless you are the owner of a working brain 
+################################################################################################
+discordWebhook = "https://discord.com/api/webhooks... webhook stuff" # ONLY THING U NEED TO EDIT 
+################################################################################################
+################# DONT TOUCH ANYTHING BELOW THIS UNLESS U HAVE A WORKING BRAIN #################
 API = "https://catalog.roblox.com/v1/search/items?category=CommunityCreations&limit=10&sortType=3&subcategory=CommunityCreations"
 infourl = "https://api.roblox.com/marketplace/productinfo?assetId="
 webhook = Webhook.from_url(discordWebhook, adapter=RequestsWebhookAdapter())
 olddata = requests.get(API).json()
-print("Scanning for new items..")
-# if somethign breaks/errors just ignore it
-while True:
+print("Scanning the roblox catalog..")
+while True: # a shitty while true loop because i am a noob
     time.sleep(5)
     newdata = requests.get(API).json()
     if newdata != olddata:
@@ -24,21 +24,28 @@ while True:
         updatedate = iteminfo["Updated"]
         if createddate[0:12] == updatedate[0:12]: #compares upload and update dates to see if it was updated in the past hour
             print(f"{itemid} is new, uploaded at {createddate}, updated at {updatedate}")
-            olddata = newdata
             # webhook stuff
             itemname = iteminfo["Name"]
             itemprice = iteminfo["PriceInRobux"]
             thumbnail = f"https://www.roblox.com/asset-thumbnail/image?assetId={itemid}&width=420&height=420&format=png"
             itemlink = "https://www.roblox.com/catalog/" + str(itemid)
-            Embed = discord.Embed(title="New UGC accessory detected!", description=f"https://www.roblox.com/catalog/{itemid}", color=0x00ff00)
+            Embed = discord.Embed(title="UGC Accessory Uploaded.", description=f"https://www.roblox.com/catalog/{itemid}", color=0x00ff00)
             Embed.add_field(name="Item Name", value=itemname, inline=True)
             Embed.add_field(name="Item Price", value=itemprice, inline=True)
             Embed.set_thumbnail(url=thumbnail)
             webhook.send(embed=Embed)
-        else:
-            print(f"{itemid} created at {createddate}, updated at {updatedate} is not new :(")
+        else: #nofity that it was only updated
+            print(f"{itemid} was updated, uploaded at {createddate}, updated at {updatedate}")
+            itemname = iteminfo["Name"]
+            itemprice = iteminfo["PriceInRobux"]
+            thumbnail = f"https://www.roblox.com/asset-thumbnail/image?assetId={itemid}&width=420&height=420&format=png"
+            itemlink = "https://www.roblox.com/catalog/" + str(itemid)
+            Embed = discord.Embed(title="UGC Accessory Updated.", description=f"https://www.roblox.com/catalog/{itemid}", color=0x00ff00)
+            Embed.add_field(name="Item Name", value=itemname, inline=True)
+            Embed.add_field(name="Item Price", value=itemprice, inline=True)
+            Embed.set_thumbnail(url=thumbnail)
+            webhook.send(embed=Embed)
     else:
-        print("No updates detected")
         continue
 # if something breaks keep the window open
 input("Press enter to exit")
